7afb1b1dd80c7d422db4976f90043ed8a1b93a0a
sofiapm <mps.sofia@gmail.com>
2017-07-31 20:52:54 +0100

    Add user model and controller.

---
+++ api/models/User.js
@@ -0,0 +1,43 @@
+/**
+ * User.js
+ *
+ * @description :: User base information: first name, last name.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    // e.g. John
+    firstName: {
+      type: 'string',
+      required: true
+    },
+
+    // e.g. Doe
+
+    lastName: {
+      type: 'string',
+      required: true
+    },
+
+    // e.g. john.doe@email.com
+
+    email: {
+      type: 'string',
+      email: true,
+      required: true,
+      unique: true
+    },
+
+    /**
+     * Attribute methods
+     */
+
+    getFullName: function (){
+      return this.firstName + ' ' + this.lastName;
+    }
+  }
+};
+
